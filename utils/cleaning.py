0440f1921afce42fab9d937a199367d60630bcd1
Pieter Van Hoefs <98814968+pietervanhoefs@users.noreply.github.com>
2022-06-15 12:13:30 +0200

    Update cleaning.py

--- utils/cleaning.py
+++ utils/cleaning.py
@@ -1,8 +1,18 @@
 from langdetect import detect_langs
 
 
-def clean(article, language="nl", dest_file=None, treshold=0.99, write_text=False):
+def clean(article: str, language="nl", write_text=False, dest_file=None, treshold=0.99) -> str:
 
+    """ 
+    Function to remove unnecessary text from the scraped articles. Discards articles that are not in the desired language.
+    arguments and keywords:
+    article: path to textfile
+    language: desired language of article
+    write_text: True if you want to write the cleaned article to a textfile
+    dest_file: name of file where the cleaned article gets written to (if write_text=True)
+    treshold: if the certainty that the language of the article is the same as the desired language is lower than this treshold, the article gts discarded
+    """
+    
     with open(article, "r") as f:
         text = f.read().split(" ")
 
